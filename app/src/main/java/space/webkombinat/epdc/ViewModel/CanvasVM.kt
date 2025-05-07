8e8d09e30ae63adb97977457794367fdedb72a93
bunnmei <kombinat.web@gmail.com>
2025-05-07 21:05:37 +0900

    app iconの追加

--- app/src/main/java/space/webkombinat/epdc/ViewModel/CanvasVM.kt
+++ app/src/main/java/space/webkombinat/epdc/ViewModel/CanvasVM.kt
@@ -13,7 +13,6 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.parcelize.IgnoredOnParcel
